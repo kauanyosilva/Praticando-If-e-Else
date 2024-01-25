e92843d16482c1bc604085d0ed37fb012b9d9e57
kauanyosilva <kauanyoliveiradasilvaa@gmail.com>
2024-01-25 16:58:40 -0300

    Questão-03

---
+++ Questão-03.js
@@ -0,0 +1,27 @@
+
+let numerodia= prompt("digite um numero de 1 a 7 para o dia da semana");
+
+if(numerodia ==1){
+console.log("Domingo");
+}
+else if(numerodia ==2){
+console.log("Segunda Feira");
+}
+else if(numerodia ==3){
+console.log("Terça Feira");
+}
+else if (numerodia ==4){
+console.log("Quarta Feira");
+}
+else if( numerodia ==5){
+console.log("Quinta Feira");
+}
+else if (numerodia ==6){
+console.log("Sexta Feira");
+}
+else if(numerodia ==7){
+console.log("Sabado");
+}
+ else {
+ console.log("Numero invalido. Por favor, insira um numero de 1 a 7.");
+ }
