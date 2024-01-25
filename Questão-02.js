4a81146bd43d303e5945ccc829268e6bc0a0c5e4
kauanyosilva <kauanyoliveiradasilvaa@gmail.com>
2024-01-25 16:57:27 -0300

    Questão-02

---
+++ Questão-02.js
@@ -0,0 +1,12 @@
+let num1 =prompt ('digite o pimeiro numero');
+let num2 =prompt("digite o segundo numero ");
+
+if (num1>num2){
+console.log("o primeiro numero e maior");
+}
+else if (num1==num2){
+console.log("os numeros sao iguais");
+}
+else {
+console.log("o segundo numero e maior");
+}
