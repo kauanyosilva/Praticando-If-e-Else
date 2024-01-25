e13095cfdca012e0afe3b04eac313be1f210f1bf
kauanyosilva <kauanyoliveiradasilvaa@gmail.com>
2024-01-25 17:24:15 -0300

    Questão-04

---
+++ Questão-04.js
@@ -0,0 +1,14 @@
+let nota = prompt("digite a nota de 0 a 100")
+
+if (nota <60){
+console.log("Reprovado");
+}
+else if (nota <=74){
+console.log("Regular");
+}
+else if( nota <=89){
+console.log("Bom");
+}
+else {
+console.log("Excelente");
+}
